c59071ee5f264803ab88d7277a52c61493667955
Cliff Calist <cliffstersprog@gmail.com>
2025-07-24 17:26:54 +0200

    refactor(loading-screen): change class and method visibility to internal

--- Runtime/SceneBoot/LoadingScreenProvider.cs
+++ Runtime/SceneBoot/LoadingScreenProvider.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace WhiteArrow.Bootstraping
 {
-    public static class LoadingScreenProvider
+    internal static class LoadingScreenProvider
     {
-        public static ILoadingScreen Screen { get; private set; }
+        internal static ILoadingScreen Screen { get; private set; }
 
-        public static void SetScreen(ILoadingScreen screen)
+        internal static void SetScreen(ILoadingScreen screen)
         {
             Screen = screen ?? throw new ArgumentNullException(nameof(screen));
             Screen.MarkAsDontDestroyOnLoad();
