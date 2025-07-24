0f0595af51593a27dd43d987e0b684e2b87ad88a
Cliff Calist <cliffstersprog@gmail.com>
2025-07-24 02:57:08 +0200

    refactor(boot-settings): encapsulate fields with properties and update access modifiers

--- Runtime/Settings/BootSettings.cs
+++ Runtime/Settings/BootSettings.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace WhiteArrow.Bootstraping
 {
     public class BootSettings : ScriptableObject
     {
-        public bool IsEnabled = true;
-        public List<string> ModuleTypeNames = new();
+        [SerializeField] private bool _isEnabled = true;
+        [SerializeField] private List<string> _moduleTypeNames = new();
         [SerializeField] private InterfaceField<ILoadingScreen> _loadingScreen;
 
 
@@ -15,6 +16,22 @@ namespace WhiteArrow.Bootstraping
 
 
 
-        public ILoadingScreen LoadingScreen => _loadingScreen.Value;
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            internal set => _isEnabled = value;
+        }
+
+        public IReadOnlyList<string> ModuleTypeNames
+        {
+            get => _moduleTypeNames;
+            internal set => _moduleTypeNames = value.ToList();
+        }
+
+        public ILoadingScreen LoadingScreen
+        {
+            get => _loadingScreen.Value;
+            internal set => _loadingScreen.Value = value;
+        }
     }
 }
